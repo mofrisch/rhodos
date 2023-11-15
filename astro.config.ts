552b0edf7a785c154b7a3a5ee1eef338b98aebef
Moritz Frisch <mo_68@gmx.de>
2023-11-15 17:33:30 +0100

    back to static

--- astro.config.ts
+++ astro.config.ts
@@ -3,26 +3,11 @@ import tailwind from "@astrojs/tailwind";
 
 import sitemap from "@astrojs/sitemap";
 import { SITE } from "./src/config";
-import cloudflare from "@astrojs/cloudflare";
+// import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   site: SITE.website,
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-
-    sitemap(),
-  ],
-
-  vite: {
-    optimizeDeps: {
-      exclude: ["@resvg/resvg-js"],
-    },
-  },
-  scopedStyleStrategy: "where",
-  //output: "static",
-  output: "server",
-  adapter: cloudflare(),
+  integrations: [sitemap(), tailwind()],
+  output: "static",
 });
