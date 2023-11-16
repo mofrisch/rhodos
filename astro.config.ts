b2bad940f49022e624ffaeb6ef3be84e666b8237
Moritz Frisch <mo_68@gmx.de>
2023-11-16 17:25:28 +0100

    server again

--- astro.config.ts
+++ astro.config.ts
@@ -2,8 +2,12 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import { SITE } from "./src/config";
+import cloudflare from "@astrojs/cloudflare";
+
+// https://astro.build/config
 export default defineConfig({
   site: SITE.website,
   integrations: [sitemap(), tailwind()],
-  output: "static",
+  output: "server",
+  adapter: cloudflare(),
 });
