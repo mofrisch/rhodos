a00cf8c0fc7dee50e7e721d8ee0510a4954ac213
Moritz Frisch <mo_68@gmx.de>
2023-11-19 17:46:12 +0100

    more menu items

--- src/content/config.ts
+++ src/content/config.ts
@@ -20,5 +20,37 @@ const fish = defineCollection({
 const soups = lunch;
 const starters = lunch;
 const cheese = lunch;
+const desserts = lunch;
+const extra = lunch;
+const fish_plates = lunch;
+const grill = lunch;
+const grill_plates = lunch;
+const gyros = lunch;
+const ice = lunch;
+const kids = lunch;
+const oven = lunch;
+const rumpsteak = lunch;
+const salads = lunch;
+const schnitzel = lunch;
+const veggie = lunch;
 
-export const collections = { lunch, fish, soups, starters, cheese };
+export const collections = {
+  lunch,
+  fish,
+  soups,
+  starters,
+  cheese,
+  desserts,
+  extra,
+  fish_plates,
+  grill,
+  grill_plates,
+  gyros,
+  ice,
+  kids,
+  oven,
+  rumpsteak,
+  salads,
+  schnitzel,
+  veggie,
+};
