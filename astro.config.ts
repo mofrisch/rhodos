492988812510c9957b982ec6be29b8cdd7e73722
Moritz Frisch <mo_68@gmx.de>
2023-11-29 20:04:08 +0100

    check

--- astro.config.ts
+++ astro.config.ts
@@ -2,11 +2,7 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import { SITE } from "./src/config";
-import cloudflare from "@astrojs/cloudflare";
 
-import vercel from "@astrojs/vercel/serverless";
-
-// https://astro.build/config
 export default defineConfig({
   site: SITE.website,
   integrations: [sitemap(), tailwind()],
