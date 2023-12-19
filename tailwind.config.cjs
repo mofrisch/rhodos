8f707130c33f8cfd9f019a3fbfc8c0a20cb0513c
Moritz Frisch <mo_68@gmx.de>
2023-12-19 15:06:03 +0100

    carousel

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}", "./node_modules/flowbite/**/*.js"],
   theme: {
     screens: {
       sm: "800px",
     },
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [require("@tailwindcss/typography"), require("flowbite/plugin")],
 };
