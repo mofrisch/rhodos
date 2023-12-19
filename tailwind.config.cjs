d4bd91c9bc2717066feddfd3d95fa1efef50e5f5
Moritz Frisch <mo_68@gmx.de>
2023-12-19 17:04:38 +0100

    layout changes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}", "./node_modules/flowbite/**/*.js"],
   theme: {
     screens: {
       sm: "800px",
+    },
+    extend: {
+      fontFamily: { sans: ["Open Sans"], ...defaultTheme.fontFamily.sans },
     },
   },
   plugins: [require("@tailwindcss/typography"), require("flowbite/plugin")],
