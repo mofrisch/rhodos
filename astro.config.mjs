ba6fe529e9de9a146fdfe60619112043c8f176bc
Moritz Frisch <mo_68@gmx.de>
2025-10-28 17:22:08 +0100

    conf update
    Signed-off-by: Moritz Frisch <mo_68@gmx.de>

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,20 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import { SITE } from "./src/config";
-import cloudflare from "@astrojs/cloudflare";
+// import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
   site: SITE.website,
   integrations: [tailwind(), sitemap()],
-  output: "hybrid",
+  output: "static",
   build: {
     format: "file",
   },
-  adapter: cloudflare(),
+  //adapter: cloudflare(),
+  image: {
+    service: {
+      entry: 'astro/assets/services/compile', // Build-time only
+      // optional: config: { sharpOptions: { /* … */ } }
+    }
+  }
 });
